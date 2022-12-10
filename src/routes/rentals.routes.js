d0f8a120c10d03bb9ff489ea44834b78efa19c42
Sabta Mendes <sabtaamendes@gmail.com>
2022-12-10 09:04:47 -0400

    feat: CRUD delete rentals

--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getRentals, postRentals} from "../controllers/rentals.controller.js";
+import { deleteRentals, getRentals, postRentals} from "../controllers/rentals.controller.js";
 import { rentalsValidation } from "../middleware/rentals.middleware.js";
 
 const router = Router();
 
 router.get("/rentals", getRentals);
 router.post("/rentals", rentalsValidation, postRentals);
+router.delete("/rentals/:id", deleteRentals);
 
 export default router;
